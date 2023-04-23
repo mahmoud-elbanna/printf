59319bf366e311e2f0523d71b42064f67a341f03
Motaweh22 <m720.nagi2003@gmail.com>
2023-04-22 18:56:45 -0700

    meto4

---
+++ get_precision.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted_string_in
+ * @i: List_argumeeents
+ * @list: list_arguuments.
+ * Return: Precisiooon.
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+	int curr_ii = *i + 1;
+	int precisioon = -1;
+
+	if (format[curr_ii] != '.')
+		return (precisioon);
+
+	precision = 0;
+
+	for (curr_ii += 1; format[curr_ii] != '\0'; curr_ii++)
+	{
+		if (is_digit(format[curr_ii]))
+		{
+			precisioon *= 10;
+			precisioon += format[curr_ii] - '0';
+		}
+		else if (format[curr_ii] == '*')
+		{
+			curr_ii++;
+			precisioon = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+	*i = curr_ii - 1;
+	return (precisioon);
+}
