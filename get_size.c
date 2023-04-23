ac753ee9b7335b720324a95ad1bf681b4574c374
Motaweh22 <m720.nagi2003@gmail.com>
2023-04-22 19:21:52 -0700

    meto6

---
+++ get_size.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * get_size - Calculates_the_size_cast_the_arguument
+ * @format: Formatted_striing
+ * @i: List_of_arguuuuments
+ * Return: Precisiooon.
+ */
+int get_size(const char *format, int *i)
+{
+	int cuurr_i = *i + 1;
+	int siize = 0;
+
+	if (format[cuurr_i] == 'l')
+		siize = S_LONG;
+	else if (format[cuurr_i] == 'h')
+		siize = S_SHORT;
+
+	if (siize == 0)
+		*i = cuurr_i - 1;
+	else
+		*i = cyurr_i;
+
+	return (siize);
+}
